aec2aad99e77d4047a8019a1e8afd2f024efb18f
Vigox <vigox@vigoxatelier.com>
2020-05-03 01:35:26 -0600

    4to Proyecto - Definir estructura base del proceso de datos.

--- 04-CuartoProyecto-CotizarSeguro/js/app.js
+++ 04-CuartoProyecto-CotizarSeguro/js/app.js
@@ -1,13 +1,63 @@
 /*=============================================>>>>>
-= CONSTRUCTOR DE COTIZADOR =
+= CONSTRUCTORES DE COTIZADOR =
 ===============================================>>>>>*/
-function Seguro(){
-
+/**
+** CONSTRUCTOR PARA SEGURO
+**/
+function Seguro(marca,periodo,tipo){
+	this.marca = marca;
+	this.periodo = periodo;
+	this.tipo = tipo;
 }
 
 /**
-** GENERAR LAS OPCIONES DE AÑOS
+** CONSTRUCTOR DE INTERFAZ DE USUARIO
 **/
+function Interfaz(){
+
+}
+
+
+/*=============================================>>>>>
+= EVENT LISTENERS =
+===============================================>>>>>*/
+const formulario = document.querySelector('#cotizar-seguro');
+
+formulario.addEventListener('submit', function(e){
+	e.preventDefault();
+
+	// Obtener valor seleccionada en Marca [select > option]
+	const marca = document.querySelector('#marca');
+	const marcaSeleccionada = marca.options[marca.selectedIndex].value;
+
+	console.log(marcaSeleccionada);
+
+	// Obtener valor seleccionada en Año/Modelo [select > option]
+	const modelo = document.querySelector('#anio');
+	const modeloSeleccionado = modelo.options[modelo.selectedIndex].value;
+
+	console.log(modeloSeleccionado);
+
+	// Obtener valor seleccionada en Tipo Seguro
+	const tipoSeguro = document.querySelector('input[name="tipo"]:checked').value;
+
+	console.log(tipoSeguro);
+
+	// Crear instancia de Interfaz
+	const interfaz = new Interfaz();
+
+	// Revisar que no existan elementos vacíos
+	if( marcaSeleccionada === '' || modeloSeleccionado === '' || tipoSeguro === '' ){
+		// Interfaz Imprimiendo un error
+		console.log("I'm sorry Dave, I'm afraid I can't do that");
+	} else {
+		// Instanciar Seguro y mostrar interfaz
+		console.log('All according to keikaku');
+	}
+
+
+});
+
 
 const max = new Date().getFullYear(),
 	  min = max - 20;
@@ -27,3 +77,7 @@ for(let i = max; i > min; i--){
 	option.innerHTML = i;
 	selectPeriodos.appendChild(option);
 }
+
+/**
+** POBLAR AÑOS EN EL SELECT
+**/
