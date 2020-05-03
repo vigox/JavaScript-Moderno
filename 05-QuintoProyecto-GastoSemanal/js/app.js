8a6769c9d64b13d14c34118c1cae377c65817daf
Vigox <vigox@vigoxatelier.com>
2020-05-03 06:42:13 -0600

    5to Proyecto - mostrar error en caso de haber campos vacíos.

--- 05-QuintoProyecto-GastoSemanal/js/app.js
+++ 05-QuintoProyecto-GastoSemanal/js/app.js
@@ -3,6 +3,7 @@
 ===============================================>>>>>*/
 const presupuestoUsuario = prompt('¿cuál es tu presupuesto semanal?');
 let cantidadPresupuesto;
+const formularioAgregarGasto = document.querySelector('#agregar-gasto');
 
 // console.log(presupuestoUsuario);
 /*=============================================>>>>>
@@ -27,6 +28,7 @@ class Presupuesto {
 ** CLASE DE INTERFAZ DE USUARIO
 **/
 class Interfaz{
+	// Procesar presupuesto obtenido
 	insertarPresupuesto(cantidadBase){
 		console.log(cantidadBase);
 		console.log('As expected');
@@ -39,12 +41,43 @@ class Interfaz{
 		presupuestoHTML.innerHTML = `${cantidadBase}`;
 		restanteHTML.innerHTML = `${cantidadBase}`;
 	}
+
+	// Procesar mensajes en la interfaz (HTML)
+	imprimirMensaje(contenidoMensaje, tipoMensaje) {
+		// contenedor del mensaje
+		const divMensaje = document.createElement('div');
+		divMensaje.classList.add('text-center', 'alert');
+
+		// Evaluar que tipo de mensaje se recibe
+		if( tipoMensaje === 'error' ) {
+			divMensaje.classList.add('alert-danger');
+		} else {
+			divMensaje.classList.add('alert-success');
+		}
+
+		divMensaje.appendChild( document.createTextNode( contenidoMensaje ) );
+
+		// Insertar mensaje en el DOM
+		document.querySelector('.primario').insertBefore(divMensaje, formularioAgregarGasto);
+
+		// Ocultar mensaje después de un tiempo y resetear formulario
+		setTimeout( function() {
+			document.querySelector('.primario .alert').remove();
+
+			formularioAgregarGasto.reset();
+
+		}, 2500 );
+
+	}
 }
 
 /*=============================================>>>>>
 = EVENT LISTENERS =
 ===============================================>>>>>*/
-// Verificar que el usuario haya puesto un presupuesto y que no sea un string vacio
+
+/**
+** VERIFICAR QUE SE INTRODUZCA UN PRESUPUESTO Y SEA VALIDO
+**/
 if(presupuestoUsuario === null || presupuestoUsuario === '') {
 	console.log("I'm sorry Dave, I'm afraid I can't do that");
 	window.location.reload();
@@ -59,3 +92,29 @@ if(presupuestoUsuario === null || presupuestoUsuario === '') {
 	console.log(cantidadPresupuesto);
 	console.log('all according to keikaku');
 }
+
+/**
+** EVENT LISTENER PARA ENVIO DE FORMULARIO
+**/
+formularioAgregarGasto.addEventListener('submit', function(e){
+	e.preventDefault();
+
+	// Leer información del formulario de gastos
+	const nombreGasto = document.querySelector('#gasto').value;
+	const cantidadGasto = document.querySelector('#cantidad').value;
+
+	// Instanciar la interfaz para llevar el registro de gastos y actualizacion del presupuesto
+	const ui = new Interfaz();
+
+	// Comprobar que los gastos no estén vacíos
+	if( nombreGasto === '' || cantidadGasto === '' ) {
+
+		// Parametros: Contenido de Mensaje y Tipo de Mensaje
+		ui.imprimirMensaje('Ocurrió un error', 'error');
+
+		console.log("I'm sorry Dave, I'm afraid I can't do that");
+	} else {
+		console.log('Dizque enviado');
+	}
+
+});
