7d1cda152917abc8bce9056e8fd162ddb3eef499
Vigox <vigox@vigoxatelier.com>
2020-05-05 09:15:11 -0600

    7to Proyecto - Inicializar Proyecto y crear estructura base.

---
+++ 07-CotizarCriptomonedas/js/app.js
@@ -0,0 +1,36 @@
+/*=============================================>>>>>
+= VARIABLES =
+===============================================>>>>>*/
+const formulario = document.querySelector('#formulario');
+
+
+/*=============================================>>>>>
+= LISTENERS =
+===============================================>>>>>*/
+formulario.addEventListener('submit', procesarFormulario);
+
+
+/*=============================================>>>>>
+= FUNCIONES / METODOS =
+===============================================>>>>>*/
+function procesarFormulario(e) {
+	e.preventDefault();
+	console.log('dood');
+
+	// Moneda real seleccionada
+	const moneda = document.querySelector('#moneda');
+	const monedaS = moneda.options[moneda.selectedIndex].value;
+
+	// Moneda virtual seleccionada
+	const cripto = document.querySelector('#criptomoneda');
+	const criptoS = cripto.options[cripto.selectedIndex].value;
+
+	console.log(monedaS, criptoS);
+
+	// Comprobar que moneda y cripto tengan valores
+	if(monedaS === '' || criptoS === '') {
+		console.log(`I'm sorry Dave, I'm afraid I can't do that`);
+	} else {
+		console.log(`All according to keikaku`);
+	}
+}
