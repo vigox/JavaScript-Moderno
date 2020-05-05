6209d374b3dcfbcdd0ac6b90575b4f126fc380b7
Vigox <vigox@vigoxatelier.com>
2020-05-05 14:14:49 -0600

    7mo Proyecto - Mostrar resultados con spinner de carga y terminar.

--- 07-CotizarCriptomonedas/js/ui.js
+++ 07-CotizarCriptomonedas/js/ui.js
@@ -72,10 +72,55 @@ class Interfaz{
 	// Se necesitan 3 valores dado al formato que maneja la API
 	mostrarResultados(resultados, monedaReal, criptoMoneda) {
 		// Pasar valores dinamicamente de Moneda / Cripto, para obtener un objeto más compacto y que se puede navegar más fácil
-		
+
 		// console.log(resultados);
 		// console.log(resultados[criptoMoneda]);
-		console.log(resultados[criptoMoneda][monedaReal])
+		// console.log(resultados[criptoMoneda][monedaReal]);
+
+		// En caso que existan un resultado previo
+		const datoAnterior = document.querySelector('#resultado > div');
+
+		if(datoAnterior){
+			datoAnterior.remove();
+		}
+
+		const datosMoneda = resultados[criptoMoneda][monedaReal];
+
+		// Recortar digitos decimales
+		let precioCripto = datosMoneda.PRICE.toFixed(2),
+		incremento = datosMoneda.CHANGEPCTDAY.toFixed(5),
+		fechaCambio = new Date(datosMoneda.LASTUPDATE * 1000).toLocaleDateString('es-MX');
+
+		const divInfoMoneda = document.querySelector('#resultado');
+
+		// construir template para el DOM
+		let datosHTML = `
+			<div class="card bg-warning">
+				<div class="card-body text-light">
+				<h2 class="card-title">Resultado:</h2>
+				<p>El precio de ${datosMoneda.FROMSYMBOL} en ${datosMoneda.TOSYMBOL} es $ ${precioCripto}</p>
+
+				<p>Variación del último día: ${incremento}%</p>
+				<p>Última actualización: ${fechaCambio}</p>
+				</div>
+			</div>
+		`;
+
+		// Mostrar spinner de cargando resultados
+		this.toggleSpinner('block');
+
+		// insertar resultado al DOM
+		setTimeout(() => {
+			divInfoMoneda.innerHTML = datosHTML;
+			this.toggleSpinner('none');
+		}, 2500);
 	}
+
+	/*----------- MOSTRAR SPINNER / CARGANDO -----------*/
+	toggleSpinner(estado) {
+		const spinner = document.querySelector('.contenido-spinner');
+		spinner.style.display = estado;
+	}
+
 
 }
